d47fc4e2618eb31c4091a5e54eedaada3fd72cd6
HiZQP <1395204174@qq.com>
2025-06-02 20:51:28 +0800

    添加项目文件。

---
+++ What's this/Source/Game/main/Game.cpp
@@ -0,0 +1,32 @@
+#include "Game.h"
+
+bool Game::game_isRunning = false;
+
+Game::Game()
+{
+}
+
+Game::~Game()
+{
+}
+
+bool Game::init(const char* title, int width, int height)
+{
+	return false;
+}
+
+void Game::clean()
+{
+}
+
+void Game::update()
+{
+}
+
+void Game::render()
+{
+}
+
+void Game::handleEvents()
+{
+}
---
+++ What's this/Source/Game/main/Game.h
@@ -0,0 +1,25 @@
+#include <string>
+#pragma once
+class Game
+{
+private:
+	static bool game_isRunning;
+	static int main_windowWidth;
+	static int main_windowHeight;
+	static const char* main_windowTitle;
+public:
+	
+	Game();
+	~Game();
+	// 初始化游戏窗口(窗口名称输入，宽度输入，高度输入)
+	static bool init(const char* title, int width, int height);
+	// 释放游戏资源
+	static void clean();
+	// 更新数据
+	static void update();
+	// 渲染
+	static void render();
+	// 事件处理
+	static void handleEvents();
+};
+
